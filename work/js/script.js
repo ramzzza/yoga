166a98ce67637af1ac3980fe6dcbc98b29bfc6cd
Reutoff <ramzzza88@gmail.com>
2019-09-27 19:20:08 +0400

    делаю слайдер

---
+++ work/js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('.top-slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>'
+  });
+ });
